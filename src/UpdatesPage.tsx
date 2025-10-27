41597a59674c58eb3b7b2b16387a54064e3c6221
ismellllll <ghostriderjuniorr@gmail.com>
2025-10-27 23:31:53 +0100

    Add files via upload

--- src/UpdatesPage.tsx
+++ src/UpdatesPage.tsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom";
 import { useEffect, useRef, useState, useCallback } from "react";
-import { useAuthUser, AuthGateModal } from "./auth";
 import { motion, AnimatePresence } from "framer-motion";
 import {
   collection,
