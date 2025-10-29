6ec244ab8129e8b82ca41f01dfbb465ea88b58c0
ismellllll <ghostriderjuniorr@gmail.com>
2025-10-29 04:20:50 +0100

    Add files via upload

--- src/JoinPage.tsx
+++ src/JoinPage.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import { Link } from "react-router-dom";
 import { MessageCircleHeart, Users, Shield, Sparkles, ArrowRight } from "lucide-react";
